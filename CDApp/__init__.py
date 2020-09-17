ea3222f8e4c4509a23e38fcf9eeedf669aca3af3
Gil Emmanuel Bancud <gembancud@gmail.com>
2020-09-17 14:15:52 +0800

    Modified CDApp for proper Socketing

--- CDApp/__init__.py
+++ CDApp/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, Response, request
-# from SuperCamera import SuperCamera
-from flask_socketio import SocketIO
-# from SuperCamera.camera_opencv import Camera
 from engineio.payload import Payload
+from flask_socketio import SocketIO
 
 Payload.max_decode_packets = 50
 app = Flask(__name__)
@@ -12,11 +10,11 @@ socketio = SocketIO(app, async_mode="eventlet")
 def index():
     return render_template('index.html')
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+# def gen(camera):
+#     while True:
+#         frame = camera.get_frame()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 # @app.route('/video_feed')
 # def video_feed():
@@ -27,11 +25,13 @@ def gen(camera):
 def connect_web():
     print('[INFO] Web client connected: {}'.format(request.sid))
     socketio.emit('enable_camera', {}, namespace='/nano')
+    socketio.emit('enable_detection', {}, namespace='/cd')
 
 @socketio.on('disconnect', namespace='/web')
 def disconnect_web():
     print('[INFO] Web client disconnected: {}'.format(request.sid))
     socketio.emit('disable_camera', {}, namespace='/nano')
+    socketio.emit('disable_detection', {}, namespace='/cd')
 
 @socketio.on('connect', namespace='/nano')
 def connect_nano():
@@ -41,7 +41,20 @@ def connect_nano():
 def disconnect_nano():
    print('[INFO] nano client disconnected: {}'.format(request.sid))
 
+@socketio.on('connect', namespace='/cd')
+def connect_nano():
+   print('[INFO] Cheat Detection client connected: {}'.format(request.sid))
+
+@socketio.on('disconnect', namespace='/cd')
+def disconnect_nano():
+   print('[INFO] Cheat Detection disconnected: {}'.format(request.sid))
+
 @socketio.on('nano2server')
 def handle_nano_message(message):
-    socketio.emit('server2web', message, namespace='/web')
+    socketio.emit('push_to_imageQueue', message, namespace='/cd')
+    # socketio.emit('change_web_image', message, namespace='/web')
+
+@socketio.on('cd2server')
+def handle_nano_message(message):
+    socketio.emit('change_web_image', message, namespace='/web')
 
