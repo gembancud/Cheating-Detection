8944978799a68a055b2e545fbebe62774c3efcee
Gil Emmanuel Bancud <gembancud@gmail.com>
2020-09-17 14:10:47 +0800

    Monkey Patch Temporarily Removed due to Slowness

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
+from CDApp import app,socketio
 import eventlet
 import eventlet.wsgi
-eventlet.monkey_patch()
-from CDApp import app,socketio
+# eventlet.monkey_patch()
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', debug=True)
