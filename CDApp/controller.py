752fee42d8c7cf01fb77c23bee3f26c5e61cc4c2
Gil Emmanuel Bancud <gembancud@gmail.com>
2020-09-24 16:09:05 +0800

    Maximized retries for Server Netgear

--- CDApp/controller.py
+++ CDApp/controller.py
@@ -1,9 +1,11 @@
 from vidgear.gears import NetGear
 
+options = {'max_retries':1000, } 
+
 class Controller:
     enabled = False
     isfocused = False
-    server = NetGear(receive_mode=True, logging=True)
+    server = NetGear(receive_mode=True, logging=True, **options)
     generatePose = False
     detectCheat = False
 
