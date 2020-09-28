43fa1b471c3712519a5c8e46aa778b652215ba71
Gil Emmanuel Bancud <gembancud@gmail.com>
2020-09-28 18:36:24 +0800

    Modified to work with CSI Camera on Jetson Nano

--- runNanoVidgear.py
+++ runNanoVidgear.py
@@ -3,6 +3,33 @@ from vidgear.gears import NetGear
 # from vidgear.gears import helper
 import cv2
 
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
 
 options = {'max_retries':1000, } 
 
@@ -11,7 +38,9 @@ client = NetGear( receive_mode = False, pattern=2,  **options)
 
 # open any valid video stream(for e.g `test.mp4` file)
 # stream = VideoGear(source='./sample.mp4').start()
-stream = cv2.VideoCapture(0)
+# stream = cv2.VideoCapture(0)
+print(gstreamer_pipeline(flip_method=0))
+stream = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 
 # loop over
 while True:
