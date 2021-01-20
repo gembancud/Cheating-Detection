a1af590d3c7bb8758a9f39b64319ac9e04438f93
Gil Emmanuel Bancud <gembancud@gmail.com>
2021-01-20 13:45:56 +0800

    Added args for Nano module

--- runNanoVidgear.py
+++ runNanoVidgear.py
@@ -2,6 +2,7 @@
 from vidgear.gears import NetGear, VideoGear
 # from vidgear.gears import helper
 import cv2
+import argparse
 
 
 def gstreamer_pipeline(
@@ -34,32 +35,47 @@ def gstreamer_pipeline(
 
 options = {'max_retries': 1000, }
 
-# define Netgear Client with `receive_mode = True` and default parameter
-client = NetGear(receive_mode=False, pattern=2,  **options)
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Description of your program')
+    parser.add_argument('-a', '--address',
+                        help='address of server', default=None)
+    parser.add_argument('-d', '--device',
+                        help='desktop or nano', default='desktop')
+
+    args = parser.parse_args()
+
+    # define Netgear Client with `receive_mode = True` and default parameter
+    client = NetGear(address=args.address, receive_mode=False,
+                     pattern=2,  **options)
+
+    # Run NanoVidGear Video
+    # stream = VideoGear(source='./sample.mp4').start()
+
+    # Run NanoVidgear PC Webcam
+    if args.device == "desktop":
+        stream = cv2.VideoCapture(0)
+
+    elif args.device == "nano":
+        stream = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+
+    # loop over
+    while True:
+        # receive frames from network
+        (grabbed, frame) = stream.read()
+        # check for received frame if Nonetype
+        if frame is None:
+            break
+        # {do something with the frame here}
+        # frame =helper.reducer(frame,50)
+        client.send(frame)
+
+    stream.stop()
+
+    # safely close client
+    client.close()
 
 
-# Run NanoVidGear Video
-# stream = VideoGear(source='./sample.mp4').start()
-
-# Run NanoVidgear PC Webcam
-stream = cv2.VideoCapture(0)
-
-# Run NanoVidgear Nano Webcam
-# print(gstreamer_pipeline(flip_method=0))
-# stream = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
-
-# loop over
-while True:
-    # receive frames from network
-    (grabbed, frame) = stream.read()
-    # check for received frame if Nonetype
-    if frame is None:
-        break
-    # {do something with the frame here}
-    # frame =helper.reducer(frame,50)
-    client.send(frame)
-
-stream.stop()
-
-# safely close client
-client.close()
+if __name__ == "__main__":
+    main()
